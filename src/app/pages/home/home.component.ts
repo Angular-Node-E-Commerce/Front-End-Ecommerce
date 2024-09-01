1211581be9643b32b5e7f2f522a6709f1a436889
farahmahfouz <farahmahfouz11@gmail.com>
2024-09-01 21:48:00 +0300

    updated ahh forms validations

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,14 +2,19 @@ import { Component } from '@angular/core';
 import { TrendingSectionComponent } from "../../components/trending-section/trending-section.component";
 import { TopCategoriesSectionComponent } from "../../components/top-categories-section/top-categories-section.component";
 import { LandingSectionComponent } from "../../components/landing-section/landing-section.component";
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ TrendingSectionComponent, TopCategoriesSectionComponent, LandingSectionComponent],
+  imports: [ TrendingSectionComponent, TopCategoriesSectionComponent, LandingSectionComponent, NgIf, NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  isLoading = false;
+ngOnInit(){
+  this.isLoading = true;
 
+}
 }
